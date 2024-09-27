01e001a63234160613b7bf1de7bab9559009d95d
soyoung <54787512+jch1999@users.noreply.github.com>
2024-09-27 19:53:09 +0900

    0927_Unreal CPP 기본 문법 학습 중
    UPROPERTY의 사용법, 생성자의 실행 타이밍(빌드한 후에 에디터에 들어오는 순간), 오류가 나서 열리지 않는 프로젝트를 고치는 방법, 로그 출력  등을 배웠다.

--- README.md
+++
@@ -1,2 +0,0 @@
-# U04_BasicSynteaxCPP
- 언리얼 C++ 기초 문법
--- Source/BasicSyntaxCPP/01_Property/CProperty.cpp
+++ Source/BasicSyntaxCPP/01_Property/CProperty.cpp
@@ -4,13 +4,22 @@ ACProperty::ACProperty()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	NumB = 999;
+
+	// When this construct is called -> After Build
+	GLog->Log("ACProperty::ACProperty()");
 }
 
 void ACProperty::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	GLog->Log("HelloWorld!");
+	FString Message;
+	/*Message.Append("NUMA = ");
+	Message.Append(FString::FromInt(NumA));*/
+
+	Message = FString::Printf(TEXT("NumA = %d, NumB = %d, NumC = %d"), NumA, NumB, NumC);
+	GEngine->AddOnScreenDebugMessage(-1,5.f,FColor::White,Message);
 }
 
 void ACProperty::Tick(float DeltaTime)
